96f32a6bd9bcb33590d7d1f1ed13cb1091ab9bf0
tutich <pkoinange02@gmail.com>
2024-04-10 01:57:05 +0300

    about jsx

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
                 <h1>What’s different about Manage?</h1>
                 <p>Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams.</p>
             </div>
+            
             <div>
                 <span>01</span>
                 <h3>Track company-wide progress</h3>
@@ -16,15 +17,15 @@ const About = () => {
                 </p>
 
                 <span>02</span>
-                <h3>Track company-wide progress</h3>
-                <p>See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to 
-                    the smallest of details. Never lose sight of the bigger picture again.
+                <h3>Advanced built-in reports</h3>
+                <p>Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the 
+                    reports you need to keep key stakeholders informed.
                 </p>
 
                 <span>03</span>
-                <h3>Track company-wide progress</h3>
-                <p>See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to 
-                    the smallest of details. Never lose sight of the bigger picture again.
+                <h3> Everything you need in one place</h3>
+                <p>Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an 
+                    all-in-one team productivity solution.
                 </p>
             </div>
         </section>
